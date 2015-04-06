8eeed070eb6de9d543a9ed803e1087dede49ca4b
Paul Williamson <squarefrog@gmail.com>
2015-04-06 15:01:21 +0100

    Set light status bar

--- TraktTopTen/App/AppDelegate.swift
+++ TraktTopTen/App/AppDelegate.swift
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
+        UIApplication.sharedApplication().statusBarStyle = .LightContent
+        
         var navigationBarAppearace = UINavigationBar.appearance()
         navigationBarAppearace.tintColor = UIColor.applicationWhiteColour()
         navigationBarAppearace.barTintColor = UIColor.applicationMainThemeColor()
