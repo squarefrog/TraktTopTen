0e6dbc9485a332195c3e6ccc949d0876306cf74e
Paul Williamson <squarefrog@gmail.com>
2015-04-07 20:13:17 +0100

    Ensure we have a url before downloading

--- TraktTopTen/Models/CollectionViewDataSource.swift
+++ TraktTopTen/Models/CollectionViewDataSource.swift
@@ -35,8 +35,10 @@ class CollectionViewDataSource : NSObject, UICollectionViewDataSource {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as MediaItemCell
         let movie = self.items[indexPath.row]
         cell.textLabel.text = "\(movie.title)"
-        let url = NSURL(string: movie.banner)
-        cell.imageView.hnk_setImageFromURL(url)
+        
+        if let url = NSURL(string: movie.banner) {
+            cell.imageView.hnk_setImageFromURL(url)
+        }
         
         return cell
     }
