d7502ad25bdb16e53a4556c1f9d87a8bbb1fa39a
Paul Williamson <squarefrog@gmail.com>
2015-04-08 20:26:16 +0100

    Cleanup TraktAPIManager

--- TraktTopTen/Networking/TraktAPIManager.swift
+++ TraktTopTen/Networking/TraktAPIManager.swift
@@ -13,31 +13,22 @@ let TraktAPIKey = "YOUR-API-KEY"
 
 class TraktAPIManager {
     func fetchTopMovies(callback: (NSData?, String?) -> Void) {
-        assert(TraktAPIKey != "YOUR-API-KEY", "You need to add your API key in line 12")
+        assert(TraktAPIKey != "YOUR-API-KEY", "Add your API key above")
         
+        let url = NSURL(string: UrlString)!
+        let request = topMoviesURLRequest(url)
         var session = NSURLSession.sharedSession()
-        
-        if let url = NSURL(string: UrlString) {
-            let request = topMoviesURLRequest(url)
-            var task = session.dataTaskWithRequest(request) {
-                (data, response, error) -> Void in
-                if error != nil {
-                    callback(nil, error.localizedDescription)
-                } else {
-                    if let httpResponse = response as? NSHTTPURLResponse {
-                        if httpResponse.statusCode != 200 {
-                            let statusCode = httpResponse.statusCode
-                            callback(nil, "Error: API returned \(statusCode)")
-                        } else {
-                            callback(data, nil)
-                        }
-                    }
-                }
+        var task = session.dataTaskWithRequest(request) {
+            (data, response, error) -> Void in
+            let httpResponse = response as NSHTTPURLResponse
+            
+            if error != nil {
+                callback(nil, "Error \(httpResponse): \(error.localizedDescription)")
+            } else {
+                callback(data, nil)
             }
-            task.resume()
-        } else {
-            callback(nil, "Error creating Trakt URL")
         }
+        task.resume()
     }
     
     func topMoviesURLRequest(url: NSURL) -> NSMutableURLRequest {
