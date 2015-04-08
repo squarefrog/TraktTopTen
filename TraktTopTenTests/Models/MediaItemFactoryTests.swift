710b903ab59ac202e8edcf5219b4d8e8af4f0ade
Paul Williamson <squarefrog@gmail.com>
2015-04-08 20:19:53 +0100

    Minor changes to media item factory tests

--- TraktTopTenTests/Models/MediaItemFactoryTests.swift
+++ TraktTopTenTests/Models/MediaItemFactoryTests.swift
@@ -17,14 +17,13 @@ class MediaItemFactoryTests: XCTestCase {
             let items = MediaItemFactory().createMediaItems(data)
             XCTAssert(items.count == 10, "Method should return the correct amout of results")
             
-            let firstItem = items.first!
+            let firstItem = items[0]
             XCTAssert(firstItem.title == "The Shawshank Redemption", "Item title not set")
             XCTAssert(firstItem.year == 1994, "Item year not set")
             XCTAssert(firstItem.slug == "the-shawshank-redemption-1994", "Item slug not set")
             XCTAssert(firstItem.banner == "https://walter.trakt.us/images/movies/000/000/234/banners/original/fce1409e26.jpg", "Item banner not set")
             
             let secondItem = items[1]
-            
             XCTAssert(secondItem.title == "Goodfellas", "Item title not set")
             XCTAssert(secondItem.year == 1990, "Item year not set")
             XCTAssert(secondItem.slug == "goodfellas-1990", "Item slug not set")
