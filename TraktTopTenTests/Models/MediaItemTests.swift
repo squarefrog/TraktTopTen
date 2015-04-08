f4246b94eb45c59b2cdbcdcac8eda0adb8faa99e
Paul Williamson <squarefrog@gmail.com>
2015-04-08 20:19:29 +0100

    Rename movie -> media item

--- TraktTopTenTests/Models/MediaItemTests.swift
+++ TraktTopTenTests/Models/MediaItemTests.swift
@@ -13,10 +13,10 @@ class MediaItemTests: XCTestCase {
 
     func testMovieCanBeInitialised() {
         let mediaItem = MediaItem(title: "Bacon", year: 1980, slug: "bacon", banner: "bacon-movie.jpg")
-        XCTAssertNotNil(mediaItem, "Movie should not be nil")
-        XCTAssert(mediaItem.title == "Bacon", "Movie title not set");
-        XCTAssert(mediaItem.year == 1980, "Movie year not set");
-        XCTAssert(mediaItem.slug == "bacon", "Movie slug not set");
+        XCTAssertNotNil(mediaItem, "MediaItem should not be nil")
+        XCTAssert(mediaItem.title == "Bacon", "MediaItem title not set");
+        XCTAssert(mediaItem.year == 1980, "MediaItem year not set");
+        XCTAssert(mediaItem.slug == "bacon", "MediaItem slug not set");
         XCTAssert(mediaItem.banner == "bacon-movie.jpg", "Movie banner url not set");
     }
 
